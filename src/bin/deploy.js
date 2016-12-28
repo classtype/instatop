5ce321b40b220366b3f6c8d1ec46801646cf5f9d
classtype <classtype@mail.ru>
2016-12-28 23:03:02 +0000

    "update"

--- src/bin/deploy.js
+++ src/bin/deploy.js
@@ -1,15 +1,54 @@
 //--------------------------------------------------------------------------------------------------
 
+var spawn = require('child_process').spawn;
+var async = require('async');
+
+//--------------------------------------------------------------------------------------------------
+
+var exec = function(args, callback) {
+    var command = args.shift();
+    var child = spawn(command, args);
+    child.on('close', function (code) {
+        callback(null, command);
+    });
+};
+
+//--------------------------------------------------------------------------------------------------
+
+async.map([
+    ['git', 'add', '.'],
+    ['git', 'commit', '-m', '"update"'],
+    ['git', 'push']], exec, function(err, r) {
+    console.log('Results:');
+    console.log(r);
+});
+
+/*
+async.series([
+    function(callback) {
+        exec('git', ['status'], callback);
+    },
+    function(callback) {
+        exec('git', ['status'], callback);
+    },
+    function(callback) {
+        exec('git', ['status'], callback);
+    }
+]);
+*/
+//--------------------------------------------------------------------------------------------------
+/*
 var colors = require('colors/safe');
 const exec = require('child_process').exec;
-
+*/
 /*--------------------------------------------------------------------------------------------------
 |
 | -> Загрузка на GitHub
 |
 |-------------------------------------------------------------------------------------------------*/
-
-exec('clear && git add . && git commit -m "update" && git push', function(error, stdout, stderr) {
+/*
+//exec('clear && git add . && git commit -m "update" && git push', function(error, stdout, stderr) {
+exec('git', function(error, stdout, stderr) {
     if (error) {
         process.stdout.write(colors.red(error));
         return;
@@ -17,5 +56,5 @@ exec('clear && git add . && git commit -m "update" && git push', function(error,
     process.stdout.write(colors.green(stdout.toString()));
     process.stdout.write(colors.red(stderr.toString()));
 });
-
+*/
 //--------------------------------------------------------------------------------------------------
