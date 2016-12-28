63bfa9a8f0143b1843cb3b07eddec507b74b51bd
classtype <classtype@mail.ru>
2016-12-28 20:27:44 +0000

    update

--- src/bin/deploy.js
+++ src/bin/deploy.js
@@ -6,7 +6,7 @@ var colors = require('colors/safe');
 //--------------------------------------------------------------------------------------------------
 
 const exec = require('child_process').exec;
-exec('git add . && git commit -m "update" && git push', function(error, stdout, stderr) {
+exec('clear && git add . && git commit -m "update" && git push', function(error, stdout, stderr) {
     if (error) {
         process.stdout.write(colors.red(error));
         return;
