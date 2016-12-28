d6ab84338887e9b44aad08c6573bc2d00bcf9e4b
classtype <classtype@mail.ru>
2016-12-28 20:27:17 +0000

    update

--- src/bin/deploy.js
+++ src/bin/deploy.js
@@ -1,14 +1,18 @@
-/*
+//--------------------------------------------------------------------------------------------------
+
 var colors = require('colors/safe');
-process.stdout.write(colors.red(data.toString()));
-*/
+//process.stdout.write(colors.red(data.toString()));
+
+//--------------------------------------------------------------------------------------------------
 
 const exec = require('child_process').exec;
-exec('git add . && git commit -m "update" && git push', (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
+exec('git add . && git commit -m "update" && git push', function(error, stdout, stderr) {
+    if (error) {
+        process.stdout.write(colors.red(error));
+        return;
+    }
+    process.stdout.write(colors.green(stdout.toString()));
+    process.stdout.write(colors.red(stderr.toString()));
 });
+
+//--------------------------------------------------------------------------------------------------
