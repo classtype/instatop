c75b7fbeb63f617266eea6e738d9f9c0798361e9
classtype <classtype@mail.ru>
2016-12-30 02:16:54 +0000

    "update"

--- src/bin/server.js
+++ src/bin/server.js
@@ -1,40 +1,23 @@
 //--------------------------------------------------------------------------------------------------
 
-var spawn = require('child_process').spawn;
 var colors = require('colors/safe');
 var exec = require('./exec');
 var ssh = require('./ssh');
+var request = require('request');
 
 //--------------------------------------------------------------------------------------------------
 
     if (process.argv[2] == 'restart') {
         var restart = function() {
-        // Init
-            var ch = spawn('ping', ['95.213.229.167', '-c', '1']);
-            
-        // Error
-            ch.on('error', function(error) {
-                console.log(colors.bgRed('Ошибка: "'+ error +'"'));
-                process.exit();
-            });
-            
-        // StdOut
-            ch.stdout.on('data', function(data) {
-                console.log(colors.green('Сервер доступен!'));
-                process.exit();
-            });
-            
-        // StdErr
-            ch.stderr.on('data', function(data) {
+            request('95.213.229.167', function (error, response, body) {
+                if (!error) {
+                    console.log(colors.green('Сервер доступен!'));
+                    process.exit();
+                }
                 console.log(colors.red('Сервер не доступен.'));
-            });
-            
-        // Close
-            ch.on('close', function(code) {
                 setTimeout(restart, 1000);
-            });
+            })
         };
-        
         
     // Очищаем консоль
         exec([['clear']], function() {
@@ -46,7 +29,7 @@ var ssh = require('./ssh');
                 password: 'yesqzgf5ej'
             },[
                 "echo '#!/bin/sh -e' > /etc/rc.local",
-                "echo 'forever start /var/projects/test/index.js' >> /etc/rc.local",
+                "echo 'forever start /var/projects/instatop/index.js' >> /etc/rc.local",
                 "echo 'exit 0' >> /etc/rc.local",
                 'shutdown -r now'
             ], function() {
