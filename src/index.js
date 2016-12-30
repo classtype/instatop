cbf4d49e9a3194d20b319a5b71c19bfa03784652
classtype <classtype@mail.ru>
2016-12-30 19:19:58 +0000

    "update"

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
+var config = require('./bin/config');
+var mysql = require('mysql');
 var http = require('http');
+
+var connection = mysql.createConnection({
+    host: 'example.org',
+    user: 'bob',
+    password: 'secret'
+});
+
+var sql = 'Mysql none.';
+
+connection.connect(function(err) {
+    if (err) {
+        sql = 'error connecting: '+ err.stack;
+        return;
+    }
+    
+    sql = 'connected as id '+ connection.threadId;
+});
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('1235Всем привет!\n');
+    res.end('Всем привет!\n' + '<br>sql: '+ sql);
 }).listen(process.env.PORT||80, process.env.IP||null);
 
 console.log('Server running at.');
