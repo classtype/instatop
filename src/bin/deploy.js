64c491adbdc295bb4d18e2b936fe557806597be1
classtype <classtype@mail.ru>
2016-12-30 00:42:37 +0000

    "update"

--- src/bin/deploy.js
+++ src/bin/deploy.js
@@ -3,39 +3,29 @@
 var exec = require('./exec');
 var ssh = require('./ssh');
 
-/*--------------------------------------------------------------------------------------------------
-|
-| -> Заливаем на GitHub
-|
-|-------------------------------------------------------------------------------------------------*/
+//--------------------------------------------------------------------------------------------------
 
+// Заливаем на GitHub
 exec([
     ['clear'],
     ['git', 'add', '.'],
     ['git', 'commit', '-m', '"update"'],
     ['git', 'push']
 ], function() {
-    console.log('End.');
+// Заходим на сервер
+    ssh({
+        host: '95.213.229.167',
+        port: 22,
+        username: 'root',
+        password: 'yesqzgf5ej'
+    },[
+        'rm -rf /var/projects/instatop',
+        'mkdir -p /var/projects/instatop',
+        'git clone https://github.com/classtype/instatop.git /var/projects/instatop',
+        'cd /var/projects/instatop && npm i --unsafe-perm',
+        'forever stopall',
+        'forever start /var/projects/instatop/src/index.js'
+    ]);
 });
-
-/*--------------------------------------------------------------------------------------------------
-|
-| -> Заходим на сервер
-|
-|-------------------------------------------------------------------------------------------------*/
-
-ssh({
-    host: '95.213.229.167',
-    port: 22,
-    username: 'root',
-    password: 'yesqzgf5ej'
-},[
-    'rm -rf /var/projects/instatop',
-    'mkdir -p /var/projects/instatop',
-    'git clone https://github.com/classtype/instatop.git /var/projects/instatop',
-    'cd /var/projects/instatop && npm i --unsafe-perm',
-    'forever stopall',
-    'forever start /var/projects/instatop/src/index.js'
-]);
 
 //--------------------------------------------------------------------------------------------------
